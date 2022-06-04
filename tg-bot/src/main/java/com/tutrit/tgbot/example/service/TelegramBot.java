822260b5d3415f3961b741145a4a253e9d930d00
mikas <6666350@gmail.com>
2022-06-04 22:58:49 +0200

    adds tg bot example

---
+++ tg-bot/src/main/java/com/tutrit/tgbot/example/service/TelegramBot.java
@@ -0,0 +1,40 @@
+package com.tutrit.tgbot.example.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+@PropertySource("application.properties")
+public class TelegramBot extends TelegramLongPollingBot {
+    @Value("${bot.username}")
+    private String botUsername;
+    @Value("${bot.token}")
+    private String botToken;
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        try {
+            SendMessage sendMessage = new SendMessage();
+            sendMessage.setText("got your message: " + update.getMessage().getText());
+            sendMessage.setChatId(update.getMessage().getChatId().toString());
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+}
